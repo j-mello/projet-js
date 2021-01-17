b9f21ac8b961b8ef139ced67bee07560063c8177
j-mello <mello.julien@gmail.com>
2021-01-17 22:04:37 +0100

    Init du projet

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Hello...</title>
+
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script type="module" async src="/src/main.js"></script>
+</head>
+<body>
+<div class="container">
+    <?php echo "<h1>Hello</h1>"; ?>
+</div>
+</body>
+</html>
---
+++ app/src/main.js
@@ -0,0 +1,5 @@
+console.log('Coucou')
+import { React } from "./React/React.js";
+import { prop_access } from "./React/libs/prop_access.js";
+
+Object.prototype.prop_access = prop_access;
