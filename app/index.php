d1c9072367bd71afac5e1031a8f3d883e5e534fb
j-mello <mello.julien@gmail.com>
2021-02-11 18:34:07 +0100

    Ajout de bootstrap

--- app/index.php
+++ app/index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-    <title>Hello...</title>
+    <title>Site de démo</title>
 
     <meta charset="utf-8">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script type="module" async src="/src/main.js"></script>
 </head>
 <body>
