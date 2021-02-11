08d8896c00c713d3e556c72a75a5a396dfa4a6f1
Jules <julesgabiam31@gmail.com>
2021-02-11 22:11:21 +0100

    ajout de la page presentation

--- app/src/Components/Root.js
+++ app/src/Components/Root.js
@@ -2,6 +2,8 @@ import { React } from "./../React/React.js";
 import { Router } from "./Router.js";
 import { Hello } from "./Hello.js";
 import { Accueil } from "./Accueil.js";
+import { Presentation } from "./Presentation.js";
+
 
 
 export class Root extends React.Component {
@@ -14,7 +16,8 @@ export class Root extends React.Component {
     {
         return React.createElement("div", null, [
            React.createElement(Router, {uri: "/Hello", component: Hello}),
-           React.createElement(Router, {uri: "/", component: Accueil})
+           React.createElement(Router, {uri: "/", component: Accueil}),
+           React.createElement(Router, {uri: "/Presentation", component: Presentation}),
         ]);
     }
 }
